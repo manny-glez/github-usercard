82969e77599d5874661d74dd59b631a5cc9cd3c1
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-06-03 20:40:51 -0500

    Met MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,30 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+const users = [
+  'ladrillo',
+  'beejjorgensen',
+  'bigknell',
+  'manny-glez',
+  'geohot'
+]
+
+function mapUsers(username) {
+  axios
+  .get(`https://api.github.com/users/${username}`)
+  .then(response => {  // YOU ONLY HAVE ACCESS TO THE DATA HERE
+    insert.appendChild( userMaker(response.data) )
+  })
+}
+
+users.map(user => {
+  mapUsers(user)
+})
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,23 +50,74 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
+*/
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
+  const insert = document.querySelector('.cards')
+
+  function userMaker(userObj) {
+
+    // creation
+    const card = document.createElement('div')
+    const userImage = document.createElement('img')
+    const cardInfo = document.createElement('div')
+    const name = document.createElement('h3')
+    const username = document.createElement('p')
+    const location = document.createElement('p')
+    const Profile = document.createElement('p')
+    const link = document.createElement('a')
+    const followers = document.createElement('p')
+    const following = document.createElement('p')
+    const bio = document.createElement('p')
+
+    // class List
+    card.classList.add('card')
+    cardInfo.classList.add('card-info')
+    name.classList.add('name')
+    username.classList.add('username')
+
+    // Content
+    userImage.src = userObj.avatar_url
+    name.textContent = userObj.name
+    username.textContent = userObj.login
+    location.textContent = userObj.location
+    Profile.textContent = 'Profile:'
+    link.href = userObj.url
+    link.textContent = 'GitHub'
+    followers.textContent = `Followers: ${userObj.followers}`
+    following.textContent = `Following: ${userObj.following}`
+    bio.textContent = `Bio: ${userObj.bio}`
+
+    // Appending
+    card.appendChild(userImage)
+    card.appendChild(cardInfo)
+    cardInfo.appendChild(name)
+    cardInfo.appendChild(username)
+    cardInfo.appendChild(location)
+    cardInfo.appendChild(location)
+    cardInfo.appendChild(Profile)
+    Profile.appendChild(link)
+    cardInfo.appendChild(followers)
+    cardInfo.appendChild(following)
+    cardInfo.appendChild(bio)
+
+    return card
+  }
+  
+/*
+    <div class="card"> 
+      <img src={image url of user} /> 
+      <div class="card-info"> 
+        <h3 class="name">{users name}</h3> 
+        <p class="username">{users user name}</p> 
+        <p>Location: {users location}</p> 
         <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
+          <a href={address to users github page}>{address to users github page}</a> 
         </p>
         <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
+        <p>Following: {users following count}</p> 
         <p>Bio: {users bio}</p>
       </div>
     </div>
